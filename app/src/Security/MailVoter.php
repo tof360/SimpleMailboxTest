dc8f6144902bf9d8d97ab52061bbf5df7f200112
tof360 <chris_1232@hotmail.com>
2023-10-24 14:12:07 +0200

    Creating voter to configure access right later

---
+++ app/src/Security/MailVoter.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Security;
+
+// src/Security/PostVoter.php
+namespace App\Security;
+
+use App\Entity\Mail;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class MailVoter extends Voter
+{
+    // these strings are just invented: you can use anything
+    const VIEW = 'view';
+    const EDIT = 'edit';
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        if (!in_array($attribute, [self::VIEW, self::EDIT])) {
+            return false;
+        }
+
+        if (!$subject instanceof Mail) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            // the user must be logged in; if not, deny access
+            return false;
+        }
+
+        // you know $subject is a Post object, thanks to `supports()`
+        /** @var Mail $mail */
+        $mail = $subject;
+
+        return match($attribute) {
+            self::VIEW => $this->canView($mail, $user),
+            self::EDIT => $this->canEdit($mail, $user),
+            default => throw new \LogicException('This code should not be reached!')
+        };
+    }
+
+    private function canView(Mail $mail, User $user): bool
+    {
+        // if they can edit, they can view
+        if ($this->canEdit($mail, $user)) {
+            return true;
+        }
+
+        // the Post object could have, for example, a method `isPrivate()`
+        return false;
+    }
+
+    private function canEdit(Mail $mail, User $user): bool
+    {
+        // this assumes that the Post object has a `getOwner()` method
+        return $user === $mail->getOwner();
+    }
+}
